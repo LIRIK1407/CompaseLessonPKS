c64290f21e72963e871b17f7ac13939abed08d60
Kirill <keril2@gmail.com>
2025-09-10 15:31:26 +0300

    Card Box

--- app/src/main/java/com/example/compaselessonpks/MainActivity.kt
+++ app/src/main/java/com/example/compaselessonpks/MainActivity.kt
@@ -1,27 +1,37 @@
 package com.example.compaselessonpks
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.compaselessonpks.ui.theme.CompaseLessonPKSTheme
-import kotlin.io.encoding.Base64
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,41 +39,76 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CompaseLessonPKSTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NextText (innerPadding)
+                Column (modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .padding(25.dp)) {
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
+                    ListItem(name = "Zac Efron", prof = "Actor")
                 }
             }
         }
     }
 }
+
 @Composable
-fun NextText(padding: PaddingValues){
-    Row (modifier = Modifier
-        .background(Color.Gray)
-        .padding(paddingValues = padding)
-        .fillMaxWidth()
-        .fillMaxHeight(1F),
-        horizontalArrangement = Arrangement.SpaceEvenly)
-    {
-    Column (
-        modifier = Modifier.background(Color.Red)
-            .fillMaxWidth(0.5F)
-            .fillMaxHeight(0.5F),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceAround) {
-        Text(text = "Hello!")
-        Text(text = "Arzamas!")
-        Text(text = "Sarov!")
+fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .clip(CircleShape)
+                        .size(64.dp)
+                )
+                Column(modifier = Modifier.padding(start = 16.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+        }
     }
-    Column (modifier = Modifier
-        .background(Color.Green)
-        .fillMaxWidth()
-        .fillMaxHeight(0.5F),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceAround) {
-        Text(text = "Welcome!")
-        Text(text = "To!")
-        Text(text = "LPK!")
-    }
-}
 }
